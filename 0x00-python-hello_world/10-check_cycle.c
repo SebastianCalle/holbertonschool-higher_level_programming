536e3622d4a8b033396b989ebde01181c2d2a587
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-09 19:54:55 +0000

    [Feature]: Function that check if list has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * check_cycle - function that check if list has a cycle
+ * @list: list to check
+ * Return: 1 if has a cycle or 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *aux1 = list;
+	listint_t *aux2 = list->next;
+
+	while (aux2 != NULL)
+	{
+		if (aux2 > aux1)
+			return (1);
+		aux2 = aux2->next;
+		aux1 = aux1->next;
+	}
+	return (0);
+}
