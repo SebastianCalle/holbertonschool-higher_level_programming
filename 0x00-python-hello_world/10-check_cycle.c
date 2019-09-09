643ea4629e6517057f88ab432f4330b347f51fdd
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-09 22:15:44 +0000

    [fix]: Solve problem whit order condition

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,11 +6,12 @@
  */
 int check_cycle(listint_t *list)
 {
+	if (list == NULL)
+		return (0);
+
 	listint_t *aux1 = list;
 	listint_t *aux2 = list->next;
 
-	if (aux1 == NULL)
-		return (0);
 	if (aux2 == aux1)
 		return(1);
 	while (aux2 != NULL)
