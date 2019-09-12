631c6e00efdaeb534a9e660f810efd7a09a4566e
Juan Sebastian Calle <jusemonca@gmail.com>
2019-09-12 17:05:45 -0500

    [Feature]: Change logic

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,19 +3,20 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
-    a = argv[1]
-    b = argv[3]
-    if len(argv) != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+    if (len(argv) != 4):
+        print("./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] == "+":
-        print(argv[1], '+', argv[3], '=', add(int(a), int(b)))
-    elif argv[2] == "-":
-        print(argv[1], '-', argv[3], '=', sub(int(a), int(b)))
-    elif argv[2] == "*":
-        print(argv[1], '*', argv[3], '=', mul(int(a), int(b)))
-    elif argv[2] == "/":
-        print(argv[1], '/', argv[3], '=', div(int(a), int(b)))
-    else:
-        print('Unknown operator. Available operators: +, -, * and /')
+    op = argv[2]
+    if (op != '+' and op != '-' and op != '*' and op != '/'):
+        print("Unknown operator. Only: +, -, * and / available")
         exit(1)
+    if (argv[2] == '+'):
+        result = add(int(argv[1]), int(argv[3]))
+    if (argv[2] == '-'):
+        result = sub(int(argv[1]), int(argv[3]))
+    if (argv[2] == '/'):
+        result = div(int(argv[1]), int(argv[3]))
+    if (argv[2] == '*'):
+        result = mul(int(argv[1]), int(argv[3]))
+    print("{} {} {} = {}".format(argv[1], argv[2],
+                                 argv[3], result))
