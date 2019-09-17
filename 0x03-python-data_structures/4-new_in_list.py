ff397e04128c73bcde6ed563fd1fbb4c1167425c
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-17 17:33:29 +0000

    [Fix] pep8 file 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list is not None and len(my_list) > 0:
-        new_list = my_list.copy()
+        new_list = my_list[:]
         if idx > len(my_list) or idx < 0:
             return new_list
         new_list[idx] = element
         return new_list
-
