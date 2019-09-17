627b39ba7c0ea10a9bfc3c7f29f30e1a7a6418aa
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-17 21:03:48 +0000

    [Feature]: Create files task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    return max(my_list)
+    a = 0
+    if my_list is not None and len(my_list) > 0:
+        for i in range(0, len(my_list)):
+            if my_list[i] > a:
+                a = my_list[i]
+        return a
+    return None
