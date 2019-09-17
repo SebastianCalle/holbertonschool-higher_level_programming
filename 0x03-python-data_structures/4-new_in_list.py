5b9cfcbf1b8c7e89d1332fb5a731ad4cba43ec47
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-17 17:32:10 +0000

    [Feature]: Create files task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is not None and len(my_list) > 0:
+        new_list = my_list.copy()
+        if idx > len(my_list) or idx < 0:
+            return new_list
+        new_list[idx] = element
+        return new_list
+
