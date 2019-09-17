4bc1eeaf6a22d83ae5e96b8426bc90106167a691
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-17 21:21:08 +0000

    [Fix]: change logic

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None and len(my_list) <= 0:
+    if my_list is None and len(my_list) == 0:
         return None
     else:
         a = my_list[0]
