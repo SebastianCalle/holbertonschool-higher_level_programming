8f669b5f6de596666a8886a97f4c25afe5360fb1
Juan Sebastian Calle <jusemonca@gmail.com>
2019-09-19 15:05:08 -0500

    [Fix] solve some checker

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 
 # function that returns a key whit the biggest integer value
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     num = 0
     for key, value in a_dictionary.items():
