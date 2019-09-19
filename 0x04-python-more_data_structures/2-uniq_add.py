763f3d8070cef9db922396da5ae7dc5cb55e3b16
Juan Sebastian Calle <jusemonca@gmail.com>
2019-09-19 12:49:51 -0500

    [Feature]: add new condition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
+
 # function that adds all unique integers in a list
 def uniq_add(my_list=[]):
-    my_list.sort()
+    if my_list is None:
+        return 0
+    new_list = my_list.copy()
+    new_list.sort()
     sum_total = 0
-    for n, i in enumerate(my_list):
-        if my_list[n-1] != i:
+    for n, i in enumerate(new_list):
+        if new_list[n-1] != i:
             sum_total += i
     return sum_total
