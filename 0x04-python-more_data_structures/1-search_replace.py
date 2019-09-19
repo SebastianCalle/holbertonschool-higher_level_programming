7a4d80ef38108237c1df9e87240aea91bb1c0479
Juan Sebastian Calle <jusemonca@gmail.com>
2019-09-19 12:33:59 -0500

    [Feature]: Function that replace elements of a list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# function that replaces all occurrences of an element by other
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for n, i in enumerate(new_list):
+        if i == search:
+            new_list[n] = replace
+    return new_list
+
