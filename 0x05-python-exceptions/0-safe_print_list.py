b5b0cac7f75e97dae7ef11e90694aa2ab1b88b59
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-30 16:09:43 +0000

    [Feature]: Add conditional if list or elemnts is null

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,8 @@
 
 # Function that prints x elements of a list
 def safe_print_list(my_list=[], x=0):
+    if not x:
+        return 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
