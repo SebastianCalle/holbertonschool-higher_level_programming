baf152ec0d1e65a65bf4742cf6462bbab4bdfb63
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-30 20:22:20 +0000

    [Fix] some checker

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 # function htat executes a function safely
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
+        result = fct(*args)
     except Exception as err:
         print("Exception:", err, file=sys.stderr)
         return None
