6278a8b9168ef3cbc4cc720326c4476c0370974b
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-30 20:07:24 +0000

    [Fix]: change print error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as err:
-        sys.stderr.write("Exception:" + str(err) + "\n")
+    except (TypeError, ValueError) as err:
+        sys.stderr.write("Exception: " + str(err) + '\n')
         return False
     return True
