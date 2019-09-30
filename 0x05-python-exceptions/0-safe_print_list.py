0d63329a8f980693b21fd54adeac212d4715f706
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-30 16:01:53 +0000

    [Feature]: Update files task 0 and README.md

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+# Function that prints x elements of a list
+def safe_print_list(my_list=[], x=0):
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            return i
+    print("")
+    return i+1 
+
