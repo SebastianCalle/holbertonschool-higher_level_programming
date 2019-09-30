55777c05b060615becac2c5fe346c843556faa85
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-30 17:09:59 +0000

    [Feature]: rotate the tail to head

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,14 @@
 
 # Function that prints x elements of a list
 def safe_print_list(my_list=[], x=0):
-    if not x:
-        return 0
-    for i in range(0, x):
+    i = 0
+    j = 0
+    while i < x:
         try:
             print("{}".format(my_list[i]), end="")
+            j += 1
+            i += 1
         except IndexError:
-            return i
+            i += 1
     print("")
-    return i+1
+    return j
