6c287b111a2590f26122de48897b82bc76157e85
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-01 17:32:20 -0500

    [Feature]: add method that print a square whit character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+class Square:
+    """Square object"""
+    def __init__(self, size=0):
+        """Inizializate class"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Return a atributte size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+        return self.__size
+
+    def area(self):
+        """Calculate area of square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print a square whit character #"""
+        if self.__size == 0:
+            print("")
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print("")
