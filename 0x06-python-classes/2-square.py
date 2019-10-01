cfba1c794dc4db875efaaac75a2fd4514e96adac
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-01 16:35:06 -0500

    [Feature]: exceptions for size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """Square object"""
+    def __init__(self, size=0):
+        """Inizializate class"""
+        self.__size = size
+        if type(self.__size) is not int:
+            print("size must be an integer", end="")
+            raise TypeError
+        if self.__size < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
