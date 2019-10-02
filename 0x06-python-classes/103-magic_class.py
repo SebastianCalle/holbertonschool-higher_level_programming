8bdb100b7299d1b79a26cad54e003b605da1710e
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-01 23:32:15 -0500

    [Fix]: correct name init

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,18 +5,18 @@ import math
 class MagicClass:
     """MagicClass class"""
 
-    def __ini__(self, radius=0):
+    def __init__(self, radius=0):
         """Inizializate class"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be number")
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
         """return area of circle"""
-        return self.__radius ** 2 * math.pi
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
         """Return circumference"""
-        return 2 * math.pi * self.__radius
+        return (2 * math.pi * self.__radius)
 
