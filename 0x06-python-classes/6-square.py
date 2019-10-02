098af786f014d53a133e5aaa6417f009d3034ce5
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-01 19:28:11 -0500

    [Fix]: condition if is one element in a tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,12 +48,11 @@ class Square:
     @position.setter
     def position(self, value):
         """Print space in position"""
-        if type(value) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) is not int or type(value[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
+        for i in range(0, len(value)):
+            if (type(value[i]) is not int or
+                    value[i] < 0 or type(value) is not tuple):
+                raise TypeError("position must be a tuple"
+                                " of 2 positive integers")
         self.__position = value
