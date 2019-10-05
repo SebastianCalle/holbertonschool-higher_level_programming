46a6eb9f678e32d8cffdf1b343ac35443ab0921d
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-05 12:32:41 -0500

    [Fix]: pep8

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,6 +7,7 @@ import numpy as np
     Function that multiplicate whit numpy a matrix
 """
 
+
 def lazy_matrix_mul(m_a, m_b):
     """function that return a matrix multiplicate"""
     return np.matmul(m_a, m_b)
