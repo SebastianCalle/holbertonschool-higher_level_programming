ed4f2b65f7075a614bc04f34be529f2392f438ee
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-10-07 21:16:54 +0000

    [Feature]: create class atributte number of instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 class Rectangle:
     """class Rectangle"""
+
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     def __str__(self):
         """str method"""
@@ -62,4 +65,5 @@ class Rectangle:
 
     def __del__(self):
         """Destructure method"""
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
