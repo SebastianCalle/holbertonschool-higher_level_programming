5ea0924489b93863e334f79ef59793f3f88ef37e
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-10-15 21:37:34 +0000

    [Feature]: Add area method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Import class BaseGeometry"""
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        """Return the string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """Return the area of Rectangle"""
+        return self.__height * self.__width
