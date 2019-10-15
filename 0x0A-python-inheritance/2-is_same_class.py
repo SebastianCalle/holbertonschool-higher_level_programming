56245a4277872ba2899ac61def6d765d0ea962c8
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-10-15 19:55:30 +0000

    [Feature]: function that return if a objects is an instances of the especific class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+    """Function that returns if the objec is an instances of class"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
