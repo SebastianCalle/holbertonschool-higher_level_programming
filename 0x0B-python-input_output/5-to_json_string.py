8354a08c35af196dfdba0c2efe9fbff19c02d650
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-10-16 19:59:35 +0000

    [Featre]: Function that returns JSON representation

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+"""Function that returns the JSON representation"""
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation"""
+    return json.dumps(my_obj)
+
