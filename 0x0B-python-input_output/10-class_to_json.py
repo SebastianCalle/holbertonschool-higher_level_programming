cd2cbd03a23c569059a1a44ebf231bb9b003025a
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-10-16 23:12:26 +0000

    [Feature]: Function that return the dictionary description

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -3,6 +3,6 @@ import json
 """Function that returns the dictionary description"""
 
 
-def class_to_jason(obj):
+def class_to_json(obj):
     """Function that returns the dictionary description"""
     return json.dumps(obj.__dict__)
