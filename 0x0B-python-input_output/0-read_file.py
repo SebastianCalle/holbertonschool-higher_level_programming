21a8c0ccbd1b9dac5e3ab21033d2923ce51b53fc
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-10-16 18:55:57 +0000

    [Fix]: solve comments and pep8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Function that read a file"""
 
 
 def read_file(filename=""):
     """Function that read a file"""
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         read_data = f.read()
         print(read_data, end="")
