fa1790603876c43a2e518099fb56275a8b3af6f1
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-15 23:47:59 -0500

    [Feature]: Use the magic methods

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(obj, name, value):
     """adds new attribute"""
-    if hasattr(obj, '__dict__'):
-        raise Exception("can't add new attribute")
+    if hasattr(obj, '__dict__') is False:
+        raise TypeError("can't add new attribute")
     else:
         setattr(obj, name, value)
