ea10f45cc00894982f8cd4c872cb0bd0d1ef354d
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-10-17 02:57:04 +0000

    [Feature]: Update attributes of class

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Class Studend
+    attributes: firts_name, second_name, age
+"""
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializate attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method to json"""
+        dict1 = self.__dict__
+        dict2 = {}
+        if type(attrs) is list:
+            for key in attrs:
+                if key in dict1:
+                    dict2[key] = dict1.get(key)
+            return dict2
+        else:
+            return dict1
+
+    def reload_from_json(self, json):
+        """Reload Json"""
+        dict1 = self.__dict__
+        keys = list(json.keys())
+        for key in keys:
+            dict1[key] = json.get(key)
+
