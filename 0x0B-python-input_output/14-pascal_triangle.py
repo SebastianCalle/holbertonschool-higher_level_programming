3a1d190c61f5875c7e5e6715f0555dd47d966694
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-17 00:08:58 -0500

    [Feautre]: Pascal Triangle

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """Print pascal triangle"""
+    line = []
+    if n > 0:
+        line.append([1])
+        for j in range(0, n - 1):
+            i = -1
+            op = []
+            while i < len(line[j]) - 1:
+                if i == -1:
+                    op.append(1)
+                else:
+                    op.append(line[j][i] +
+                                     line[j][i + 1])
+                i += 1
+            op.append(1)
+            line.append(op)
+    return line
