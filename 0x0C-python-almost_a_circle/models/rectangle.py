b49f7c4e6dd072748d4bed9ea1f1f83694dfc0c4
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-18 15:40:30 -0500

    [Feature]: create display method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,3 +70,11 @@ class Rectangle(Base):
     def area(self):
         """Method area"""
         return self.__width * self.__height
+
+    def display(self):
+        """Display the Rectangle"""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
