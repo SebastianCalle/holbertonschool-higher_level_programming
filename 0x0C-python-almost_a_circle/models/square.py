e6b015b4fa806a424b295df66c88c1f3a722d387
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-19 00:11:52 -0500

    [Feature]: add getter and setter size attribute

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,9 +6,21 @@ from .rectangle import Rectangle
 class Square(Rectangle):
     """Class Square"""
     def __init__(self, size, x=0, y=0, id=None):
+        self.__size = size
         super().__init__(id=id, x=x, y=y, width=size, height=size)
 
     def __str__(self):
         """Return string"""
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                   self.width))
+
+    @property
+    def size(self):
+        """Return private attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter size attribute"""
+        self.width = value
+        self.height = value
