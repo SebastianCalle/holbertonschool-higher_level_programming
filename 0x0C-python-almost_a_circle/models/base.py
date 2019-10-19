511620415a8c6e2e06e217b1b6c1e45d95dc8eeb
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-19 13:00:43 -0500

    [Feature]: create staticmethod that return strign from dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
+import json
+"""Module Python"""
+
+"""Base of the modules"""
+
+
 class Base:
     """Class Base"""
 
     __nb_objects = 0
+
     def __init__(self, id=None):
         """Initializate attributes"""
         if id is not None:
@@ -12,3 +19,13 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return JSON representation"""
+        if not isinstance(
+                list_dictionaries,
+                list) and list_dictionaries is None or len(list_dictionaries) < 1:
+            return "[]"
+        else:
+            string = json.dumps(list_dictionaries)
+            return string
