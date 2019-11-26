2eeb4ce80b985e7850309ac5cef0437bb39710b7
Juan Sebastian Calle <jusemonca@gmail.com>
2019-11-26 12:16:24 -0500

    [Feature]: script that creates the MySQL server user user_0d_1.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS user_0d_1@localhosts IDENTIFIED BY 'user_0d_1_pwd';
+-- Give privileges
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
