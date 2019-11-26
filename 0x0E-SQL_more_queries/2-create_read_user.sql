5c65ebfc4be946b821a2efd2390384858f35969a
Juan Sebastian Calle <jusemonca@gmail.com>
2019-11-26 12:18:16 -0500

    [Feauture]: script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,5 +5,3 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhosts IDENTIFIED BY 'user_0d_2_pwd';
 -- Give privileges
 GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhosts` IDENTIFIED BY 'User_0d_2_pwd';
--- Reload privileges
-FLUSH PRIVILEGES;
