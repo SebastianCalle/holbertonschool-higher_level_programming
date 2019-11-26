bb9913e8bf68eb4e1069e1f69c128b7ae1c8a083
Juan Sebastian Calle <jusemonca@gmail.com>
2019-11-26 00:59:02 -0500

    [Feature]: script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
+-- SHOW PRIVILEGES USER-1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- SHOW PRIVILEGES USER-2
+SHOW GRANTS FOR user_0d_2@localhost;
