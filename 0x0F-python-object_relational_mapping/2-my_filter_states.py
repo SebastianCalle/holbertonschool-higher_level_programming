6bee25be00dca7136aefaa29a0167bfbbe5de548
Juan Sebastian Calle <jusemonca@gmail.com>
2019-12-14 13:16:12 -0500

    [Feature]: script thtat takes in an argument and display all values

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that lists states that doing match
+"""
+import MySQLdb
+from sys import argv
+
+
+def print_states():
+    """
+    Print states of databease order by id
+    """
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id;")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    db.close()
+
+if __name__ == '__main__':
+    print_states()
