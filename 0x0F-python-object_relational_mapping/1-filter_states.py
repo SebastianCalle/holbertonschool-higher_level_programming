e765ea5f3e7e03eddadbf1768e7f2979062949d2
Juan Sebastian Calle <jusemonca@gmail.com>
2019-12-14 13:06:42 -0500

    [Feature]: Scriptthat lists all states with a name starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that lists states that start with N
+"""
+import MySQLdb
+from sys import argv
+
+
+def print_states():
+    """
+    Print states of databease order by id
+    """
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE name REGEXP '^[N].*$' ORDER BY id;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+if __name__ == '__main__':
+    print_states()
