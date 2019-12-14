2a1b4bd066c625775f357bc2af642ed905f3a2a0
Juan Sebastian Calle <jusemonca@gmail.com>
2019-12-14 12:54:54 -0500

    [Feature]: Create readme and script that lists all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from database
+"""
+import MySQLdb
+from sys import argv
+
+def print_states():
+    """
+    Print states of databease order by id
+    """
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+if __name__=='__main__':
+    print_states()
