76d3110b2dee9681b286493963331dc0e956b9dd
Juan Sebastian Calle <jusemonca@gmail.com>
2019-12-14 23:14:09 -0500

    [Feature]: Script that lists all State from databes

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,8 @@ from model_state import Base, State
 import sqlalchemy as db
 
 if __name__ == "__main__":
-    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     conection = engine.connect()
     metadata = db.MetaData()
     state = db.Table('states', metadata, autoload=True, autoload_with=engine)
