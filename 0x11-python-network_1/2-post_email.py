b4641f91b3f42d3544e63a84885ddc797e024662
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-01-13 22:15:06 +0000

    [Feature]: Script that sends an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,13 +6,14 @@ import urllib.parse
 import sys
 
 
-url = sys.argv[1]
-email = sys.argv[2]
-values = {'email': email}
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {'email': email}
 
-data = urllib.parse.urlencode(values)
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    page = response.read()
-    print(page)
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print(page)
