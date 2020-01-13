6509dc20a4c87aa7fdd171f8a7a96bd0ba5948fa
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-01-13 22:58:47 +0000

    [Feature]: Script htat takes in a URL, handle error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 # Script that takes in a URL, handle error
-
 import urllib.request
 import urllib.error
 import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    req = url
+    req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.URLError as e:
-        print("Error code: ", e.getcode())
+        print("Error code: {}".format(e.code))
