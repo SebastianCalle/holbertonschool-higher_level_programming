9d2e4b55cc99e48e12aef334b69b8fc4d393267f
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-01-13 21:04:10 +0000

    [Feature]:  script that takes in a URL, sends a request to the URL

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,11 +2,10 @@
 # Script that takes in a URL, sends a request to the URL
 
 import urllib.request
-import urllib.parse
 import sys
 
 
 url = sys.argv[1]
 req = url
 with urllib.request.urlopen(req) as response:
-    print(response.headers['X-Request-Id'])
+    print(response.headers.get('X-Request-Id'))
