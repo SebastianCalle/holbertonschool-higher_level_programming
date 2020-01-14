bcbcf0b4ed1745dd99119d7acdcdfe0ab5542926
Juan Sebastian Calle <jusemonca@gmail.com>
2020-01-13 20:29:54 -0500

    [Feature]: Script that fethces URL

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,9 @@
 
 import requests
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print('\t - type: {}'.format(type(r.text)))
-    print('\t - content: {}'.format(r.text))
+    print('Body response:')
+    print('\t- type:', type(r.text))
+    print('\t- content:', r.text)
