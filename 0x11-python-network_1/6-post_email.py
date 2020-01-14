256ff84853df8586130a3779aed18464a73d782a
Juan Sebastian Calle <jusemonca@gmail.com>
2020-01-13 20:47:51 -0500

    [Feature]: Script that send POST

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Script that takes in a URL and an email address, send POST
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+
+    r = requests.post(url, data = payload)
+    print(r.text)
