f7576598a0888b979687082ecd89dee1a31067f0
Juan Sebastian Calle <jusemonca@gmail.com>
2020-01-13 22:37:25 -0500

    [Feature]: Script that takes your Github credentials

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     url = 'https://api.github.com/user'
-    r = requests.get(url, auth = (user, password))
+    r = requests.get(url, auth=(user, password))
     result = r.json()
     print(result.get('id'))
