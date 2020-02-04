2af653db3d3165483d4ab7a207ef88f4de580c64
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-04 17:42:46 +0000

    [Fix]: space identation

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Script that verificated if pass arguments
 const args = process.argv.slice(2);
-if (args.length === 1) {
+if (args) {
   console.log(args[0]);
 } else {
   console.log('No argument');
