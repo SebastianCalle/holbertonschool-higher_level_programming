18437db9e7d853e7b4bb32e7d92e2a4e16a6bb04
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-04 18:09:16 +0000

    [Featiure]: Script that print statements with loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// loops for print mesagges
+const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let x = 0; x < args.length; x++) {
+  console.log(args[x]);
+}
