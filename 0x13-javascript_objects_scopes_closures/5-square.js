f2d31054b72ef0b64e57a4e6a8f276da7f0865d9
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-10 19:59:43 +0000

    [Feature]: Crereate square class extend of rectangle

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+// Empty class Rectangle
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    let x;
+    for (let i = 0; i < this.height; i++) {
+      x = '';
+      for (let j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
