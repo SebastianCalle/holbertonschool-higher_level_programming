f366b988c5530ebb46d562e00d2dc57948ed278d
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-10 17:57:55 +0000

    [Feature]: Create method constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Empty class Rectangle
+class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+module.exports = Rectangle;
