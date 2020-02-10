d6f4e6a8f2a9e29e1f7a58ec79e6d6df15815751
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-10 23:17:41 +0000

    [Feature]: Create method for square class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,17 @@
 #!/usr/bin/node
 // Empty class Square
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
-    if (c ===  undefined) {
-      c = 'X';
+    if (c == undefined) {
+      c = 'X'
+      console.log(c);
     }
-    for (let i = 0; i < this.size; i++) {
-      let a = '';
-      for (let j = 0; j < this.size; j++) {
-        a += c;
+    for (let i = 0; i < this.width; i++) {
+      let x = '';
+      for (let j = 0; j < this.width; j++) {
+        x += c;
       }
-      console.log(a);
+      console.log(x);
     }
   }
 };
