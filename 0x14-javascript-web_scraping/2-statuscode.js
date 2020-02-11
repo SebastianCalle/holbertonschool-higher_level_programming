724feab2d165eb513e5db90d7fccaa085793a261
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-11 18:22:29 +0000

    [Feature]: Script that display the status of a get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script that displaya the status code of a GET request
+const request = require('request');
+const path = process.argv[2];
+request(path, function (error, response) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    console.log('Code:', response && response.statusCode);
+  }
+});
