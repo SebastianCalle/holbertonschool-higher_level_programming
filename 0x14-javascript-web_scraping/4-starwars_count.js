5e5c578d6687224940f584ee7595545b1d6271c6
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-11 20:58:37 +0000

    [Feature]: Count the movies of a character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Script that prints the title of Start Wars movie
+const request = require('request');
+const path = process.argv[2];
+request(path, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    const character = 'https://swapi.co/api/people/18/';
+    const list = [];
+    const dict = JSON.parse(body);
+    for (const i in dict.results) {
+      if (dict.results[i].characters.includes(character)) {
+        list.push(i);
+      }
+    }
+    console.log(list.length);
+  }
+});
