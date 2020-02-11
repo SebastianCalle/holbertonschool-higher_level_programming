6953d6acb79271d4e80b26896cfe1f5d785aa46c
Juan Sebastian Calle <jusemonca@gmail.com>
2020-02-10 22:37:55 -0500

    [Feature]: Script that import a list and create new array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Function that imports an array and computes a new array
 const list = require('./100-data').list;
-console.log(list)
+console.log(list);
 let array = [];
 array = list.map((x, i) => x * i);
 console.log(array);
