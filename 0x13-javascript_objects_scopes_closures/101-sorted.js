c2d0bab11b83358596bb56d56a0e86698fc9f31d
Juan Sebastian Calle <jusemonca@gmail.com>
2020-02-10 23:15:40 -0500

    [Feature]: Create new dic

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,9 +3,6 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 for (const i in dict) {
-  console.log(i);
-  console.log(dict[i]);
-  console.log(newDict[dict[i]]);
   if (newDict[dict[i]] === undefined) {
     newDict[dict[i]] = [];
   }
