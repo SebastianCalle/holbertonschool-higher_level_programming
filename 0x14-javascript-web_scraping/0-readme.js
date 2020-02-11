2dbfedd1c70aba3cebce0c4fee88276f0258ed89
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-11 17:33:38 +0000

    [Feature]: Create readme and script that read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,5 +3,9 @@
 const fs = require('fs');
 const file = process.argv[2];
 fs.readFile(file, 'utf8', function (err, data) {
-  err ? console.error(err) : console.log(data);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
