4fda3573c344fa9c7f8fd7a4a217e8254baf57e0
Juan Sebastian Calle <jusemonca@gmail.com>
2020-02-11 22:51:39 -0500

    [Feature]: Script that print characters in order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// Script that prints the title of Start Wars movie
+const request = require('request');
+const path = 'https://swapi.co/api/films/' + process.argv[2];
+const ids = {};
+request.get(path, function (error, response, body, cache = 'no-cache') {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    const list = JSON.parse(body).characters;
+    list.forEach(elem => {
+      request.get(elem, function (error, response, body, cache = 'no-cache') {
+        if (error) {
+          console.log('error:', error);
+        } else {
+          const val = JSON.parse(body).name;
+          const key = JSON.parse(body).url.split('/')[5];
+          ids[key] = val;
+        }
+        const lenIds = Object.getOwnPropertyNames(ids);
+        if (lenIds.length === list.length) {
+          for (const k in ids) {
+            console.log(ids[k]);
+          }
+        }
+      });
+    });
+  }
+});
