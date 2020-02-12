022928821517205fbf4de0d7a47f6937eee068b0
Juan Sebastian Calle <jusemonca@gmail.com>
2020-02-11 21:18:19 -0500

    [Fix]: Change request

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,14 +2,14 @@
 // Script that prints the title of Start Wars movie
 const request = require('request');
 const path = 'https://swapi.co/api/films/' + process.argv[2];
-request(path, function (error, response, body) {
+request.get(path, function (error, response, body) {
   if (error) {
     console.log('error:', error);
   } else {
     const request1 = require('request');
     const list = JSON.parse(body).characters;
     list.forEach(elem => {
-      request1(elem, function (error, response, body) {
+      request.get(elem, function (error, response, body) {
         if (error) {
           console.log('error:', error);
         } else {
