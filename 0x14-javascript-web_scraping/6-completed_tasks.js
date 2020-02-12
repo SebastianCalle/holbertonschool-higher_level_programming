bee454be7f7a520d15a6191a096724245f47fa4a
Juan Sebastian Calle <jusemonca@gmail.com>
2020-02-11 21:00:49 -0500

    [Feature]: Change logic of loop

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,21 +9,15 @@ request(url, function (error, response, body) {
   } else {
     const list = JSON.parse(body);
     const dict = {};
-    let count = 0;
-    let id = 1;
-    for (let i = 1; i <= list.length - 1; i++) {
-      if (id === list[i].userId && list[i].completed === true) {
-        if (dict[id] === undefined) {
-          dict[id] = 0;
+    list.forEach(elem => {
+      if (elem.completed === true) {
+        if (dict[elem.userId] === undefined) {
+          dict[elem.userId] = 1
+        } else {
+          dict[elem.userId]++;
         }
-        count += 1;
-        dict[id] = count;
       }
-      if (list[i].userId > id) {
-        id += 1;
-        count = 0;
-      }
-    }
+    });
     console.log(dict);
   }
 });
