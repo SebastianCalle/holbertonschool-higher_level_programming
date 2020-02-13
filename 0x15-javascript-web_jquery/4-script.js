40a878c99fa083456128526e32f970955079b6e9
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-13 22:16:50 +0000

    [Feature]: Create toogle that change color of header

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$('div#toggle_header').click(function ToggleClass() {
+  if($('header').hasClass('green')) {
+    $('header').removeClass('green');
+    $('header').addClass('red');
+  } else {
+    $('header').removeClass('red');
+    $('header').addClass('green');
+  }
+});
