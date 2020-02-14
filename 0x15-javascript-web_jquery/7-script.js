473439f39e024c884d4b9c3b415dfe1fba43ed69
Juan Sebastian Calle <jusemonca@gmail.com>
2020-02-13 20:28:57 -0500

    [Feature]: Get a name of star wars api

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+$(function () {
+  $.get(
+    'https://swapi.co/api/people/5/?format=json',
+    function (data, textStatus) {
+      $('div#character').text(data.name);
+    },
+    "json"
+  );
+});
